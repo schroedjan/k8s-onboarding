b56ac383a603cd2632b56fed3aa4529babdb6786
Jan Schroeder <jan.schroeder@inform-software.com>
2025-05-02 15:12:39 +0200

    update deployments and namespaces

--- k8s-deployment/deploy.sh
+++ k8s-deployment/deploy.sh
@@ -1,5 +1,5 @@
-podman build -t hello-world-service .
-podman tag hello-world-service 192.168.64.4:32000/hello-world-service:latest
-podman push --tls-verify=false 192.168.64.4:32000/hello-world-service:latest
+buildah build -t hello-world-service .
+buildah tag hello-world-service localhost:32000/hello-world-service:latest
+buildah push --tls-verify=false localhost:32000/hello-world-service:latest
 kubectl apply -f deployment.yaml
-kubectl rollout restart deployment hello-world-service
+kubectl rollout restart deployment hello-world-service -n application
